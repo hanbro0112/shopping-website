a440e72f944336aaf03963395a06761ad62ec692
hanbro0112 <xx2468xx@gmail.com>
2024-05-22 11:41:43 +0800

    [Added]: add front-end template

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ const port = 8080;
 app.get('/', (req, res) => {
     res.send('hello world!');
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
