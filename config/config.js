40a7f6d013a9a44aee490a778526e48c46e5f4e2
hanbro0112 <xx2468xx@gmail.com>
2025-02-18 10:54:52 +0800

    feat: mysql 設定為 aws rds

--- config/config.js
+++ config/config.js
@@ -1,12 +1,14 @@
+const awsRds = require('./aws-rds-config');
+
 module.exports = {
     development: {
         mysql: {
-            username: 'root',
-            password: 'root',
-            database: 'db',
-            host: '172.18.1.2',
-            port: '3306',
-            dialect: 'mysql',
+            username: awsRds.username || 'root',
+            password: awsRds.password || 'root',
+            database: awsRds.database || 'db',
+            host: awsRds.host || '172.18.1.2',
+            port: awsRds.port || '3306',
+            dialect: awsRds.dialect || 'mysql',
             timezone: '+08:00',
         },
         redis: {
