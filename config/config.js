7f4cbb197af148b21451f67c2153e6b916dfa41b
hanbro0112 <xx2468xx@gmail.com>
2025-02-19 10:10:31 +0800

    feat: 調整 config 格式符合 sequelize migrate

--- config/config.js
+++ config/config.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        mysql: {
-            username: 'root',
-            password: process.env.RDS_PASSWORD || 'root',
-            database: 'db',
-            host: process.env.RDS_HOST || '172.18.1.2',
-            port: '3306',
-            dialect: 'mysql',
-            timezone: '+08:00',
-        },
+        // db migrate 預設檔案規格
+        username: 'root',
+        password: process.env.RDS_PASSWORD || 'root',
+        database: 'db',
+        host: process.env.RDS_HOST || '172.18.1.2',
+        port: '3306',
+        dialect: 'mysql',
+        timezone: '+08:00',
+
         redis: {
             host: '172.18.1.3',
             post: '6379',
@@ -20,15 +20,14 @@ module.exports = {
         },
     },
     test: {
-        mysql: {
-            username: 'root',
-            password: 'root',
-            database: 'db',
-            host: '127.0.0.1',
-            port: '3306',
-            dialect: 'mysql',
-            timezone: '+08:00',
-        },
+        username: 'root',
+        password: 'root',
+        database: 'db',
+        host: '127.0.0.1',
+        port: '3306',
+        dialect: 'mysql',
+        timezone: '+08:00',
+
         redis: {
             host: '127.0.0.1',
             post: '6379',
